7b5e47ca5492e408af7cea7c446e01e90cc7e5a4
Elra Ghifary <elraghifary@gmail.com>
2018-08-04 15:27:37 +0700

    validate account on login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -63,6 +63,10 @@ class AuthController extends Controller
 
         $credentials = request(['email', 'password']);
 
+        $credentials['active'] = 1;
+        
+        $credentials['deleted_at'] = null;
+
         if(!Auth::attempt($credentials))
             return response()->json([
                 'message' => 'Unauthorized'
